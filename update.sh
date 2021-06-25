445c816c830cb23d567351741fd146c9cda12706
mahdymirzade <me@mahdym.ir>
2021-06-25 09:09:23 +0430

    25 June 2021 - Repository Updated.

--- update.sh
+++ update.sh
@@ -21,28 +21,9 @@ bash updatepkgs.sh
 # Update Repository Database
 bash updatedb.sh
 
-# Performing a CleanUp
-echo -e "${CYAN}# Performing a Repository Clean Up...${RESET}"
-
-# Backup config & remove git dir
-mv .git/config config
-rm -rf .git
-
-# Setup git 
-echo -e "${CYAN}# Setting up git repository...${RESET}"
-git init
-git config --global user.name "mahdymirzade"
-git config --global user.email "me@mahdym.ir"
-git config --global init.defaultBranch "main"
-git config --global credential.helper cache
-git config --global credential.helper "cache --timeout=25000"
-git config --global push.default simple
-echo -e "${BLUE}# Git repository setup completed.${RESET}"
-
-# Move config to git again & push changes to repository
+# Push changes to repository
 MSG="$(date +'%d %B %Y') - Repository Updated."
-echo -e "${CYAN}# Moving config to git dir & push changes...${RESET}"
-mv config .git/config
+echo -e "${CYAN}# Pushing changes...${RESET}"
 git add -A .
 git commit -m "$MSG"
 git branch -m main
